6863c096867d4da08615264242a9b951b59106ba
Fischer <mfischer10@luc.edu>
2025-10-20 19:46:34 +0000

    Adding testing script and log output for tryhg19.in

---
+++ testing_dbNSFP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This script is to run all 5 datasets using dbNSFP database search command. 
+
+#testing
+cd /home/mfischer10/vep_project/mydbNSFP/dbNSFP5.2a
+
+/usr/bin/time -v java -cp . search_dbNSFP52a \
+  -i ~/vep_project/mydbNSFP/dbNSFP5.2a/tryhg19.in \
+  -o ~/vep_project/mydbNSFP/dbNSFP5.2a/tryhg19.out \
+  -v hg19 -w 1-7,12-18,37-155,533-543 > ~/vep_project/mydbNSFP/dbNSFP5.2a/tryhg19.log 2> ~/vep_project/mydbNSFP/dbNSFP5.2a/tryhg19.time.log &
