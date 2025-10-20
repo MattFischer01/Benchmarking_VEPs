2e5c69708e69294e3fb28f382e0af21f66fab30b
Fischer <mfischer10@luc.edu>
2025-10-20 17:48:47 +0000

    Updating formatting_input.sh script

--- formatting_input.sh
+++ formatting_input.sh
@@ -19,26 +19,47 @@
 ##Entrez:4541 
 
 #The format for my test datasets will be:
-##"chr pos ref alt"
+##"chr pos ref alt refaa altaa"
 
 #For this input format, I will need to extract fields 3,4,5,6 from my csv files, separated by commas, my input will be tab deliminated.
 mkdir ~/vep_project/testing_sets/dbnsfp_format
 
-
-#File 1:
-awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6}' ~/vep_project/testing_sets/exovar_filtered_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/exovar_filtered_tool_scores.tsv
-
-#File 1:
-awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6}' ~/vep_project/testing_sets/exovar_filtered_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/exovar_filtered_tool_scores.tsv
-
-#File 1:
-awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6}' ~/vep_project/testing_sets/exovar_filtered_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/exovar_filtered_tool_scores.tsv
-
-#File 1:
-awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6}' ~/vep_project/testing_sets/exovar_filtered_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/exovar_filtered_tool_scores.tsv
-
-#File 1:
-awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6}' ~/vep_project/testing_sets/exovar_filtered_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/exovar_filtered_tool_scores.tsv
+#This files are formatted in hg19 formatting
 
 
-#When running the dbNSFP code, I will only need the first 150 columns, which will contain all the predictions available from this tool.
+#File 1: ExoVar, Deleterious Variants: 5156, Neutral Variants: 3694
+awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6, $13, $14}' ~/vep_project/testing_sets/exovar_filtered_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/exovar_filtered_tool_scores.tsv
+
+
+#File 2: HumVar, Deleterious Variants: 21090, Neutral Variants: 19299
+awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6, $13, $14}' ~/vep_project/testing_sets/humvar_filtered_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/humvar_filtered_tool_scores.tsv
+
+
+#File 3: predictSNPSelected, Deleterious Variants: 10000, Neutral Variants: 6098, total: 16098. This file has 34 alternate coding regions (chrHG299_PATCH or chrHSCHR6_MHC_COX) that I will removem leaving 16066 snps.
+awk 'BEGIN {FS=","} {
+    if ($3 !~ /^chrH/)
+        print $0
+}' ~/vep_project/testing_sets/predictSNP_selected_tool_scores.csv > ~/vep_project/testing_sets/predictSNP_selected_tool_scores_V2.csv
+
+
+awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6, $13, $14}' ~/vep_project/testing_sets/predictSNP_selected_tool_scores_V2.csv > ~/vep_project/testing_sets/dbnsfp_format/predictSNP_selected_tool_scores.tsv
+
+#further formatting: removing chr from column 1 and changing MT to M
+sed -E 's/^chr([0-9]+)\t/\1\t/; s/^chrMT\t/M\t/; s/^chrX\t/X\t/; s/^chrY\t/Y\t/' ~/vep_project/testing_sets/dbnsfp_format/predictSNP_selected_tool_scores.tsv > ~/vep_project/testing_sets/dbnsfp_format/predictSNP_selected_tool_scores_V2.tsv
+
+
+#File 4: SwissVarSelected, Deleterious Variants: 4526, Neutral Variants: 8203, total 12,729. This file has 369 alternate coding regions (chrHG299_PATCH or chrHSCHR6_MHC_COX) that I will remove, leaving 12360 snps.
+awk 'BEGIN {FS=","} {
+    if ($3 !~ /^chrH/)
+        print $0
+}' ~/vep_project/testing_sets/swissvar_selected_tool_scores.csv > ~/vep_project/testing_sets/swissvar_selected_tool_scores_V2.csv
+
+awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6, $13, $14}' ~/vep_project/testing_sets/swissvar_selected_tool_scores_V2.csv > ~/vep_project/testing_sets/dbnsfp_format/swissvar_selected_tool_scores.tsv
+
+#further formatting: removing chr from column 1 and changing MT to M
+sed -E 's/^chr([0-9]+)\t/\1\t/; s/^chrMT\t/M\t/; s/^chrX\t/X\t/; s/^chrY\t/Y\t/' ~/vep_project/testing_sets/dbnsfp_format/swissvar_selected_tool_scores.tsv > ~/vep_project/testing_sets/dbnsfp_format/swissvar_selected_tool_scores_V2.tsv
+
+
+#File 5: VariBenchSelected, Deleterious Variants: 4309, Neutral Variants: 5957
+awk -F',' 'NR > 1 {OFS="\t"; print $3, $4, $5, $6, $13, $14}' ~/vep_project/testing_sets/varibench_selected_tool_scores.csv > ~/vep_project/testing_sets/dbnsfp_format/varibench_selected_tool_scores.tsv
+
